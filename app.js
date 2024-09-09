b763e1f2bdff141f6a11da2f7d708ce45dfecfa9
harshitha01 <harshithasukumaran@gmail.com>
2024-09-09 23:42:39 +0530

    Project created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const jsonwebtoken=require("jsonwebtoken")
+
+let app=express()
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+
+})
+
+app.listen(3030,()=>{
+    console.log("server started")
+})
